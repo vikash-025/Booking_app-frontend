4af0a579d2124b2c57c2efce963507889f69c442
vikash-025 <2020ugec025@nitjsr.ac.in>
2023-06-19 23:03:51 +0530

    baseUrlAdded

--- movies/src/index.js
+++ movies/src/index.js
@@ -8,7 +8,7 @@ import { Provider } from "react-redux";
 import { store } from "./store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-axios.defaults.baseURL = "http://localhost:5000";
+axios.defaults.baseURL = "https://booking-app-8h7k.onrender.com";
 root.render(
   <React.StrictMode>
     <BrowserRouter>
